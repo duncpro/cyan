37945011da30c4156d745392c4bc5ec6bef75cf0
Duncan <duncpro@icloud.com>
2024-11-13 01:36:22 -0500

    Fix and document `fast_hash`
    Previously `fast_hash` was incomplete and had no doc comment. Now it does.

--- cyan-compiler/src/util/bits.rs
+++ cyan-compiler/src/util/bits.rs
@@ -6,10 +6,13 @@ impl Truncate<u8> for u32 {
     fn truncate(self) -> u8 { return self as u8; }
 }
 
+/// Fast but insecure hash function taken from the JDK String#toString() method.
 pub fn fast_hash(s: &[u8]) -> usize {
     let mut hash: usize = 0;
     for i in 0..s.len() {
-        hash += (s.len() - i + 1).wrapping_mul(usize::from(s[i]));
+        hash += 31usize.wrapping_pow((s.len() - i + 1) as u32)
+            .wrapping_mul(usize::from(s[i]));
+        
     }
     return hash;
 }
